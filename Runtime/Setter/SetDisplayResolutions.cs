efae6d9824fe1a31b4b982aa0a4b4adb2dfec738
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-07-17 23:57:50 +0200

    ⠀⠀⠀⠀⠀

--- Runtime/Setter/SetDisplayResolutions.cs
+++ Runtime/Setter/SetDisplayResolutions.cs
@@ -4,18 +4,8 @@ namespace UnityEssentials
 {
     public class SetDisplayResolutions : MonoBehaviour
     {
-        private const string DataReference = "test";
-
-        private UIMenuOptionsData _optionsData;
-
         public void Awake()
         {
-            if (transform.parent.GetComponent<UIMenu>().Data
-                .GetDataByReference(DataReference, out _optionsData))
-            {
-                _optionsData.IsDynamic = true;
-                _optionsData.Options = new string[] { "Test" };
-            }
         }
     }
 }
