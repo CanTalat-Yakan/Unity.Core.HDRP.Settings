cf8d277eec6533afdfd5b1308484ea30059b1341
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-07-20 04:22:55 +0200

    ⠀⠀⠀⠀⠀

---
+++ Runtime/Video/RenderResolution/GetRenderResolution.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace UnityEssentials
+{
+    [RequireComponent(typeof(UIMenuOptionsDataConfigurator))]
+    public class GetRenderResolution : MonoBehaviour
+    {
+        [Info]
+        [SerializeField]
+        private string _info =
+            "This component retrieves all available display resolutions from the system and populates the menu options.\n" +
+            "It is intended for use with UIMenuOptionsDataConfigurator to allow users to select their preferred screen resolution.";
+
+        public void Awake()
+        {
+            var displayResolution = new string[Screen.resolutions.Length + 1];
+            for (int i = 0; i < Screen.resolutions.Length; i++)
+            {
+                var resolution = Screen.resolutions[i];
+                displayResolution[i] = $"{resolution.width}x{resolution.height}";
+            }
+            displayResolution[^1] = "Native";
+
+            GetComponent<UIMenuOptionsDataConfigurator>().Options = displayResolution;
+        }
+    }
+}
